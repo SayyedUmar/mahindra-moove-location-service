29f141e53c95f744465689c2cc68147fb78718ee
kalpesh patel <1988.kalpesh@gmail.com>
2018-02-13 11:06:09 +0530

    Updated Geofence Event struct with fields from mobile.

--- socketstore/geofence_event.go
+++ socketstore/geofence_event.go
@@ -1,17 +1,40 @@
 package socketstore
 
 import (
+	"bytes"
+	"encoding/json"
 	"time"
 )
 
 type GeofenceEvent struct {
-	GeofenceType string
-	TripID       int
-	TripRouteID  int
-	DriverID     int
-	Lat          float64
-	Lng          float64
-	Distance     int
-	Speed        float64
-	Timstamp     time.Time
+	TransitionType string
+	TripID         int
+	SiteID         int
+	TripRouteID    int
+	EmployeeID     int
+	Lat            float64
+	Lng            float64
+	Speed          float32
+	Bearing        float32
+	Accuracy       float32
+	Timestamp      time.Time
+	UnixSeconds    int64
+}
+
+//GeofenceEventFromJSON cosntructs GeofenceEvent from json event
+func GeofenceEventFromJSON(msg []byte) (*GeofenceEvent, error) {
+	var geofenceEvent GeofenceEvent
+	buffer := bytes.NewBuffer(msg)
+	decoder := json.NewDecoder(buffer)
+	err := decoder.Decode(&geofenceEvent)
+	if err != nil {
+		return nil, err
+	}
+	geofenceEvent.Timestamp = time.Unix(geofenceEvent.UnixSeconds/1000, 0)
+	return &geofenceEvent, nil
+}
+
+//IsDwellEvent returns true if the event is Dwell type otherwise returns false.
+func (ge *GeofenceEvent) IsDwellEvent() bool {
+	return ge.TransitionType == "GEOFENCE_DWELL"
 }
