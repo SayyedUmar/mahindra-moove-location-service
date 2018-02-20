bcd1f20ceba13d1921ed7bb923de7cf75a186f2d
kalpesh patel <1988.kalpesh@gmail.com>
2018-02-20 21:46:50 +0530

    Added GeofenceType, LocationType, BusStopName fields and Removed EmployeeID in GeofenceEvent struct.

--- socketstore/geofence_event.go
+++ socketstore/geofence_event.go
@@ -8,10 +8,12 @@ import (
 
 type GeofenceEvent struct {
 	TransitionType string
+	GeofenceType   string
+	LocationType   string
 	TripID         int
 	SiteID         int
 	TripRouteID    int
-	EmployeeID     int
+	BusStopName    string
 	Lat            float64
 	Lng            float64
 	Speed          float32
@@ -37,4 +39,31 @@ func GeofenceEventFromJSON(msg []byte) (*GeofenceEvent, error) {
 //IsDwellEvent returns true if the event is Dwell type otherwise returns false.
 func (ge *GeofenceEvent) IsDwellEvent() bool {
 	return ge.TransitionType == "GEOFENCE_DWELL"
+}
+
+//IsForEmployeeLocation returns true if the event is for Employee home location otherwise returns false.
+func (ge *GeofenceEvent) IsForEmployeeLocation() bool {
+	return ge.LocationType == "EMPLOYEE_HOME"
+}
+
+//IsForNodalPoint returns true if the event is for Nodal Bus stop location otherwise returns false.
+func (ge *GeofenceEvent) IsForNodalPoint() bool {
+	return ge.LocationType == "NODAL_POINT"
+}
+
+//IsForSite returns true if the event is for Site location otherwise returns false.
+func (ge *GeofenceEvent) IsForSite() bool {
+	return ge.LocationType == "SITE"
+}
+
+//IsForNarrowGeofence returns true if the event is triggered for narrow geofence area otherwise returns false.
+//On Mobile side radius of geofence is 200 meters
+func (ge *GeofenceEvent) IsForNarrowGeofence() bool {
+	return ge.GeofenceType == "Narrow"
+}
+
+//IsForWiderGeofence returns true if the event is triggered for wider geofence area otherwise returns false.
+//On Mobile side radius of geofence is 1500 meters
+func (ge *GeofenceEvent) IsForWiderGeofence() bool {
+	return ge.GeofenceType == "Wider"
 }
