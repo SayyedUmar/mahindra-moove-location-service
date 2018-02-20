3ff44055754fccc38f7861f9d2a19890bc697fe9
kalpesh patel <1988.kalpesh@gmail.com>
2018-02-20 21:46:50 +0530

    Added GeofenceType, LocationType, BusStopName fields and Removed EmployeeID in GeofenceEvent struct.

--- socketstore/geofence_event_test.go
+++ socketstore/geofence_event_test.go
@@ -9,9 +9,11 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func createGeofenceEvent(transitionType string) GeofenceEvent {
+func createGeofenceEvent(transitionType string, locationType string, geofenceType string) GeofenceEvent {
 	return GeofenceEvent{
 		TransitionType: transitionType,
+		LocationType:   locationType,
+		GeofenceType:   geofenceType,
 	}
 }
 
@@ -20,6 +22,8 @@ func TestGeofenceEventFromJSON(t *testing.T) {
 	geofenceJSONEvent := []byte(fmt.Sprintf(`
 		{
 			"transitionType":"GEOFENCE_ENTER",
+			"locationType" : "SITE",
+			"geofenceType" : "Wider",
 			"lat":13.036566,
 			"lng":80.216382,
 			"bearing":0,
@@ -29,12 +33,14 @@ func TestGeofenceEventFromJSON(t *testing.T) {
 			"tripId":36521,
 			"siteId":2,
 			"tripRouteId":442671,
-			"employeeId":21
+			"busStopName":"bus stop 1"
 		}
 	`, currentTime.Unix()))
 	geofenceEvent, err := GeofenceEventFromJSON(geofenceJSONEvent)
 	tst.FailNowOnErr(t, err)
 	assert.Equal(t, "GEOFENCE_ENTER", geofenceEvent.TransitionType)
+	assert.Equal(t, "SITE", geofenceEvent.LocationType)
+	assert.Equal(t, "Wider", geofenceEvent.GeofenceType)
 	assert.Equal(t, 13.036566, geofenceEvent.Lat)
 	assert.Equal(t, 80.216382, geofenceEvent.Lng)
 	assert.Equal(t, float32(0.0), geofenceEvent.Bearing)
@@ -44,16 +50,61 @@ func TestGeofenceEventFromJSON(t *testing.T) {
 	assert.Equal(t, 36521, geofenceEvent.TripID)
 	assert.Equal(t, 2, geofenceEvent.SiteID)
 	assert.Equal(t, 442671, geofenceEvent.TripRouteID)
-	assert.Equal(t, 21, geofenceEvent.EmployeeID)
 	//Don't know how to test time.
 	// assert.Equal(t, currentTime.Second(), geofenceEvent.Timestamp.Second())
+
+	geofenceJSONEvent = []byte(fmt.Sprintf(`
+		{
+			"transitionType":"GEOFENCE_ENTER",
+			"locationType" : "SITE",
+			"geofenceType" : "Wider",
+			"lat":13.036566,
+			"lng":80.216382,
+			"bearing":0,
+			"speed":30,
+			"accuracy":20.5,
+			"unixSeconds":%d,
+			"tripId":36521,
+			"siteId":2,
+			"tripRouteId":442671,
+			"busStopName":1
+		}
+	`, currentTime.Unix()))
+	//Giving wrong busStopName type
+	geofenceEvent, err = GeofenceEventFromJSON(geofenceJSONEvent)
+	assert.Error(t, err)
+	assert.Nil(t, geofenceEvent)
 }
 
 func TestGeofenceEvent_IsDwellEvent(t *testing.T) {
-	geofenceEvent := createGeofenceEvent("GEOFENCE_ENTER")
+	geofenceEvent := createGeofenceEvent("GEOFENCE_ENTER", "", "")
 	assert.False(t, geofenceEvent.IsDwellEvent())
-	geofenceEvent = createGeofenceEvent("GEOFENCE_DWELL")
+	geofenceEvent = createGeofenceEvent("GEOFENCE_DWELL", "", "")
 	assert.True(t, geofenceEvent.IsDwellEvent())
-	geofenceEvent = createGeofenceEvent("GEOFENCE_EXIT")
+	geofenceEvent = createGeofenceEvent("GEOFENCE_EXIT", "", "")
 	assert.False(t, geofenceEvent.IsDwellEvent())
+}
+
+func TestGeofenceEventLocationTypes(t *testing.T) {
+	geofenceEvent := createGeofenceEvent("GEOFENCE_ENTER", "SITE", "")
+	assert.False(t, geofenceEvent.IsForEmployeeLocation())
+	assert.True(t, geofenceEvent.IsForSite())
+	assert.False(t, geofenceEvent.IsForNodalPoint())
+	geofenceEvent = createGeofenceEvent("GEOFENCE_DWELL", "NODAL_POINT", "")
+	assert.False(t, geofenceEvent.IsForEmployeeLocation())
+	assert.False(t, geofenceEvent.IsForSite())
+	assert.True(t, geofenceEvent.IsForNodalPoint())
+	geofenceEvent = createGeofenceEvent("GEOFENCE_EXIT", "EMPLOYEE_HOME", "")
+	assert.True(t, geofenceEvent.IsForEmployeeLocation())
+	assert.False(t, geofenceEvent.IsForSite())
+	assert.False(t, geofenceEvent.IsForNodalPoint())
+}
+
+func TestGeofenceTypes(t *testing.T) {
+	geofenceEvent := createGeofenceEvent("GEOFENCE_ENTER", "SITE", "Wider")
+	assert.True(t, geofenceEvent.IsForWiderGeofence())
+	assert.False(t, geofenceEvent.IsForNarrowGeofence())
+	geofenceEvent = createGeofenceEvent("GEOFENCE_DWELL", "NODAL_POINT", "Narrow")
+	assert.False(t, geofenceEvent.IsForWiderGeofence())
+	assert.True(t, geofenceEvent.IsForNarrowGeofence())
 }
