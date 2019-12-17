41ba6163c64e402151c333e1c66452adae9c5080
Dhruv Sharma <sharma.dhruv@mahindra.com>
2019-12-17 14:11:30 +0530

    Reverted ENV variable for deployment

--- models/pg.go
+++ models/pg.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"os"
+
 	"github.com/MOOVE-Network/location_service/models/migrations"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
@@ -10,20 +12,20 @@ import (
 var db *sqlx.DB
 
 func InitSQLConnection() *sqlx.DB {
-	// dbUrl := os.Getenv("LOCATION_PG_DATABASE_URL")
-	// if dbUrl == "" {
-	// 	dbUrl = "postgres://localhost/location_service_dev?sslmode=disable"
-	// }
-	const (
-		host     = "moove-pg-uat10.cjny84emnsh9.ap-south-1.rds.amazonaws.com"
-		port     = 5432
-		user     = "MOOVE_DEV"
-		password = ""
-		dbname   = "moove-pg-uat10"
-	)
-	dbURL := "MOOVE_DEV:NG$Pir7ySMJ9m&p9@moove-pg-uat10.cjny84emnsh9.ap-south-1.rds.amazonaws.com/location_service?sslmode=disable"
-		//fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",host, port, user, password, dbname)
-		panic("postgres Database connecting..")
+	dbUrl := os.Getenv("LOCATION_PG_DATABASE_URL")
+	if dbUrl == "" {
+		dbUrl = "postgres://localhost/location_service_dev?sslmode=disable"
+	}
+	// const (
+	// 	host     = "moove-pg-uat10.cjny84emnsh9.ap-south-1.rds.amazonaws.com"
+	// 	port     = 5432
+	// 	user     = "MOOVE_DEV"
+	// 	password = ""
+	// 	dbname   = "moove-pg-uat10"
+	// )
+	// dbURL := "MOOVE_DEV:NG$Pir7ySMJ9m&p9@moove-pg-uat10.cjny84emnsh9.ap-south-1.rds.amazonaws.com/location_service?sslmode=disable"
+	// 	//fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable",host, port, user, password, dbname)
+	// 	panic("postgres Database connecting..")
 	localDb, err := sqlx.Open("postgres", dbURL)
 	if err != nil {
 		panic("something is wrong with open")
